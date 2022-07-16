77554769f228eab557b717198ba8812fed7a423c
Mitko Ivanov <mitko.ivanov@mymail.champlain.edu>
2022-07-16 18:56:38 -0400

    First commit
    * implemented 3rd person camera

---
+++ SummerProject/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public Transform playerTransform;
+
+    public Vector3 cameraOffset;
+
+    [Range(0.01f, 1.0f)]
+    public float smoothFactor = 0.5f;
+
+    public bool lookAtPlayer = true;
+    public bool rotateAroundPlayer = true;
+
+    public float rotationSpeed = 5.0f;//could also be called mouseSensitivity
+
+
+    void Start()
+    {
+        cameraOffset = transform.position - playerTransform.position;
+    }
+
+    void LateUpdate()
+    {
+        if (rotateAroundPlayer)
+        {
+            Quaternion camTurnAngle = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * rotationSpeed, Vector3.up);
+
+            cameraOffset = camTurnAngle * cameraOffset;
+        }
+
+
+        Vector3 newPos = playerTransform.position + cameraOffset;
+        transform.position = Vector3.Slerp(transform.position, newPos, smoothFactor);
+
+
+        if(lookAtPlayer || rotateAroundPlayer)
+        {
+            transform.LookAt(playerTransform);
+        }
+    }
+}
